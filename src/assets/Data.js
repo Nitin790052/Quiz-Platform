f83ad85b5a345c2bf6188c6402dce1f9df6a22ad
Nitin790052 <nitinkumar79005205@gmail.com>
2025-02-19 20:22:27 +0530

    project complete

---
+++ src/assets/Data.js
@@ -0,0 +1,86 @@
+ const data =[
+    {
+     question:"1. Which planet is closest to the Sun?",
+     option1:"A. Venus",
+     option2:"B. Mercury",
+     option3:"C. Earth",
+     option4:"D. Mars",
+     ans:2
+},
+{
+    question:"2. Which data structure organizes items in a First-In, First-Out (FIFO) manner?",
+    option1:"A. Stack",
+    option2:"B. Queue",
+    option3:"C. Tree",
+    option4:"D. Graph",
+     ans:2
+},
+{
+    question:"3. Which of the following is primarily used for structuring web pages?",
+    option1:"A. Python",
+    option2:"B. Java",
+    option3:"C. HTML",
+    option4:"D. C++",
+     ans:3
+},
+{
+    question:"4. Which chemical symbol stands for Gold?",
+    option1:"A. Au",
+    option2:"B. Gd",
+    option3:"C. Ag",
+    option4:"D. Pt",
+     ans:1
+},
+{
+    question:"5. Which of these processes is not typically involved in refining petroleum?",
+    option1:"A. Fractional distillation",
+    option2:"B. Cracking",
+    option3:"C. Polymerization",
+    option4:"D. Filtration",
+     ans:4
+},
+
+{
+    question:"6. What is the value of 12 + 28?",
+    option1:"A.  45",
+    option2:"B.  40",
+    option3:"C.  67",
+    option4:"D.  35",
+    ans:2
+},
+{
+    question:"7. How many states are there in the United States?",
+    option1:"A.  65",
+    option2:"B.  80",
+    option3:"C.  50",
+    option4:"D.  75",
+    ans:3
+},
+{
+    question:"8. In which year was the Declaration of Independence signed?",
+    option1:"A.  1540",
+    option2:"B.  2390",
+    option3:"C.  2127",
+    option4:"D.  1776",
+    ans:4
+},
+{
+    question:"9. What is the value of pi rounded to the nearest integer?",
+    option1:"A.  3",
+    option2:"B.  1",
+    option3:"C.  5",
+    option4:"D.  9",
+    ans:1
+},
+{
+    question:"10. If a car travels at 60 mph for 2 hours, how many miles does it travel?",
+    option1:"A.  95",
+    option2:"B.  100",
+    option3:"C.  120",
+    option4:"D.  140",
+    ans:3
+},
+ 
+]
+
+export default data;
---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,85 @@
+import React, { useRef, useState } from 'react'
+import '../Quiz/Quiz.css'
+import data from '../../assets/Data';
+
+const Quiz = () => {
+
+    let [index, setindex] = useState(0);
+    let [question, setquestion] = useState(data[index])
+    let [lock,setlock] = useState(false)
+    let [score,setscore] = useState(0)
+    let [result,setresult]= useState(false)
+
+    let option1 = useRef(null);
+    let option2 = useRef(null);
+    let option3 = useRef(null);
+    let option4 = useRef(null);
+
+   let option_array=[option1,option2,option3,option4];
+
+    const CheckAns= (e, ans) => {
+        if (lock==false) {
+             if (question.ans == ans) {
+            e.target.classList.add("correct");
+            setlock(true);
+            setscore(prev=>prev+1)
+        }
+        else {
+            e.target.classList.add("wrong");
+            setlock(true);
+            option_array[question.ans-1].current.classList.add("correct");
+        }
+        } 
+    }
+
+   const next = ()=>{
+       if (lock==true) {
+        if (index === data.length-1) {
+            setresult(true);
+            return 0;
+        }
+         setindex(++index);
+         setquestion(data[index]);
+         setlock(false);
+         option_array.map((option)=>{
+            option.current.classList.remove("wrong");
+            option.current.classList.remove("correct");
+            return null; 
+         })
+       }    
+   }
+      const reset = ()=>{
+        setindex(0);
+        setquestion(data[0]);
+        setscore(0);
+        setlock(false);
+        setresult(false);
+      }
+
+    return (
+        
+            <div className="container">
+                <h1>Quiz App</h1>
+                <hr />
+                {result?<></>:<>
+                <h2>{index + 1}.{question.question}</h2>
+                <ul>
+                    <li ref={option1} onClick={(e)=>{CheckAns(e,1)}}>{question.option1}</li>
+                    <li ref={option2} onClick={(e)=>{CheckAns(e,2)}}>{question.option2}</li>
+                    <li ref={option3} onClick={(e)=>{CheckAns(e,3)}}>{question.option3}</li>
+                    <li ref={option4} onClick={(e)=>{CheckAns(e,4)}}>{question.option4}</li>
+                </ul>
+                <button onClick={next}>Next</button>
+                <div className="index">{index+1} of {data.length} question</div>
+                </>}
+                {result?<><h2>you scored {score} out of {data.length}</h2>
+                <button onClick={reset}>Reset</button></>:<></>}
+                
+            </div>
+
+        
+
+    )
+}
+
+export default Quiz
